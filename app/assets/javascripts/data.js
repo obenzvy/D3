9d672cae4aedd1c850d692561046569baaf1e3f6
Orr Ben-Zvy <obenzvy@gmail.com>
2012-09-04 19:30:29 -0500

    initial commit

---
+++ app/assets/javascripts/data.js
@@ -0,0 +1,70 @@
+
+
+var stats = {
+  "course": "123",
+  "duration": 10,
+  "viewers": 10,
+  "huh": [
+    {
+      "user": "john",
+      "time": 2
+    },
+    {
+      "user": "john",
+      "time": 5
+    },
+    {
+      "user": "john",
+      "time": 7
+    },
+    {
+      "user": "orr",
+      "time": 5
+    },
+    {
+      "user": "orr",
+      "time": 7
+    },
+    {
+      "user": "orr",
+      "time": 7
+    },
+    {
+      "user": "orr",
+      "time": 7
+    },
+    {
+      "user": "orr",
+      "time": 7
+    },
+    {
+      "user": "orr",
+      "time": 7
+    }
+  ],
+  "aha": [
+    {
+      "user": "greg",
+      "time": 2
+    },
+    {
+      "user": "greg",
+      "time": 8
+    }
+  ],
+ "questions": [
+    {
+      "question": "How did you do that???",
+      "time": 2
+    },
+    {
+      "question": "This makes no sense???",
+      "time": 5
+    },
+    {
+      "question": "I don't get it!!",
+      "time": 8
+    }
+  ]
+
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+D3::Application.config.secret_token = 'a01300313e7d3ce4fe50fc99bab83b9e778d24bd98c873a541208795f9b9e4bc0c15eece621a38156014dd1c4cb2b0af4908494e74fdaa2478da0851bc476bb8'
